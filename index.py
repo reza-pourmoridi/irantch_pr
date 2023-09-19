f519bbce247941dff28c667deabf258f783374ba
reza <rezapourmoridi@gmail.com>
2023-09-19 09:52:30 +0330

    designing

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from recommendation import index as recom_index  # Import the recommendation/index module
+# from recommendation import index as recom_index  # Import the recommendation/index module
 from modular import modular as modular_index
 
 
@@ -11,19 +11,6 @@ def home():
     footer = render_template('footer.html')
     return header + footer
 
-@app.route('/recom-back-end', methods=['GET'])
-def recom_back_end():
-    # Execute the recommendation/index module's code for the '/recom-back-end' route
-    return recom_index.run_app()
-
-@app.route('/recom', methods=['GET'])
-def recom():
-    with open('recommendation/index.html', 'r') as index_file:
-        index_html = index_file.read()
-    header = render_template('header.html')
-    footer = render_template('footer.html')
-    full_html = header + index_html + footer
-    return full_html
 
 @app.route('/modular', methods=['GET'])
 def modular():
